03228b65da527f31c0eea6f653eaaf478733a9f4
Dusan Srbulovic <dsrbulovic@Dusans-MacBook-Pro.local>
2020-01-21 09:46:24 +0100

    Dodat middleware za auth

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -41,7 +41,11 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
+            
         ],
+        'auth' =>[
+            'auth'
+        ]
     ];
 
     /**
